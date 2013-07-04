b541042923941036825d833841ac93b773266f3d
unknown <mkumar@MKUMAR2L.ptcnet.ptc.com>
2013-07-04 17:49:42 +0530

    04Jul13_1

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ var fs = require("fs");
 var port = process.env.PORT || 5000;
 
 http.listen(port);
+//http.listen('8888');
+
+var onlineClients = {};
 
 	function onRequest(request,response) {
 	
@@ -29,17 +32,23 @@ http.listen(port);
 	
 	function onConnected(socket) {
 		
+		socket.on('getRooms', function() {
+			 var existingRooms = Object.keys(io.sockets.manager.rooms);
+			 //console.log('existing Rooms are', existingRooms);
+			 socket.emit('allRooms', existingRooms);
+		})
 		socket.on('joinRoom', function (data) {
-			console.log('Data Received: ', data);
+			//console.log('Data Received: ', data);
 			var userRoom,userNickname;
 			for(var key in data){ 
 				if(key =="room"){
 					userRoom = data[key];
-					socket.join(data[key]);
+					socket.join(userRoom);
 				}
 				else {
 					userNickname = data[key];
-					socket.set('nickname',data[key]);
+					socket.set('nickname',userNickname);
+					onlineClients[userNickname] = socket.id;
 				}
 			}
 			var nicknames="";
@@ -51,8 +60,41 @@ http.listen(port);
 						nicknames += nickname+',';
 				})
 			});
+
 			io.sockets.in(userRoom).emit('allUsers', nicknames);
 			io.sockets.in(userRoom).emit('joinRoom',userNickname);
+			
+	    });
+		
+		socket.on('unjoinRoom', function (data) {
+			console.log('in unjoin');
+			console.log('Data Received: ', data);
+			var userRoom,userNickname;
+			for(var key in data){ 
+				if(key =="room"){
+					userRoom = data[key];
+					socket.leave(userRoom);
+				}
+				else {
+					userNickname = data[key];
+				}
+			}
+			var nicknames="";
+			io.sockets.clients(userRoom).forEach(function (socket) {
+				socket.get('nickname', function(err, nickname) {
+					if(nicknames==null || nicknames=="") 
+						nicknames= nickname+',';
+					else 
+						nicknames += nickname+',';
+				})
+			});
+			
+			console.log('nicknames are '+nicknames);
+			console.log('in unjoin 1');
+			
+			io.sockets.in(userRoom).emit('allUsers', nicknames);
+			io.sockets.in(userRoom).emit('unjoinRoom',userNickname);
+			console.log('in unjoin 2');
 			
 	    });
 		
@@ -65,14 +107,20 @@ http.listen(port);
 						userRoom = data[key];
 					}
 					else {
-						userMessage ='['+name+']:'+data[key];
+						userMessage ='<b>'+name+'</b>: '+data[key];
 					}
 				}
-				console.log('Message broadcast to', userRoom);
-				console.log('Message is', userMessage);
+				//console.log('Message broadcast to', userRoom);
+				//console.log('Message is', userMessage);
 				io.sockets.in(userRoom).emit('publicMessage',userMessage);
 				//socket.broadcast.to(userRoom).emit('publicMessage',userMessage);
 			})
+		});
+		
+		socket.on('privateMessage',function (to, from, msg) {
+			//console.log('Message Received: ', msg, 'for ', to, 'from ', from);
+			var id = onlineClients[to];
+			io.sockets.socket(id).emit('privateMessage',from, msg);
 		});
 		
 		socket.on('message', function (msg) {
