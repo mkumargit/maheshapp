811fffd1a2e687041303059ddd777926adfacb20
unknown <mkumar@MKUMAR2L.ptcnet.ptc.com>
2013-07-10 18:24:09 +0530

    10Jul13_7

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,7 +1,7 @@
 var querystring = require("querystring");
 
 function start(res,req,io){
-		if(typeof(req.query['nickname']) != 'undefined' && req.query['nickname'] != null)
+		//if(typeof(req.query['nickname']) != 'undefined' && req.query['nickname'] != null)
 			res.cookie('nickname',req.query['nickname']);
 		res.sendfile(__dirname + '/public/static/test.html');
 }
