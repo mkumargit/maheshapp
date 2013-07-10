f864a9005ec0491b36c1bd827165574c8393371e
unknown <mkumar@MKUMAR2L.ptcnet.ptc.com>
2013-07-10 17:58:58 +0530

    10Jul13_2

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,7 +3,8 @@ var querystring = require("querystring");
 function start(res,req,io){
 		console.log('username is ...'+req.query['nickname']);
 		res.cookie('nickname',req.query['nickname']);
-		res.sendfile(__dirname + '/public/static/test.html');
+		//res.sendfile(__dirname + '/public/static/test.html');
+		res.sendfile('/public/static/test.html');
 }
 
 exports.start = start;
