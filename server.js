95e7b602bb5856d4a5ea6da445613de4ed90cda2
unknown <mkumar@MKUMAR2L.ptcnet.ptc.com>
2013-09-04 17:00:35 +0530

    04Sep13_8

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ app.configure(function(){
     }
 );
 
+io.enable('browser client minification');  // send minified client
+io.enable('browser client etag');          // apply etag caching logic based on version number
+io.enable('browser client gzip');          // gzip the file
+io.set('log level', 1);                    // reduce logging
+
 io.configure(function () { 
   io.set("transports", ["xhr-polling"]); 
   io.set("polling duration", 10); 
